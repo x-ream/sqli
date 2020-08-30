85200be04764c5b9beed0090964daa196da806ad
simsevenx <cooperation@qq.com>
2020-08-30 17:13:12 +0800

    opt

--- sqli-core/src/main/java/io/xream/sqli/util/SqliExceptionUtil.java
+++ sqli-core/src/main/java/io/xream/sqli/util/SqliExceptionUtil.java
@@ -11,28 +11,6 @@ public class SqliExceptionUtil {
 
     private SqliExceptionUtil(){}
 
-    public static String getMessage(Exception e){
-        String msg = e.getMessage();
-        msg += "\n";
-        StackTraceElement[] eleArr = e.getStackTrace();
-        if (eleArr == null || eleArr.length == 0)
-            return msg;
-        msg += eleArr[0].toString();
-        msg += "\n";
-        int length = eleArr.length;
-        if (eleArr != null && length > 0){
-            if (length > 2){
-                msg += eleArr[1].toString();
-                msg += "\n";
-                msg += eleArr[2].toString();
-            }else if (length > 1){
-                msg += eleArr[1].toString();
-            }
-        }
-
-        return msg;
-    }
-
     public static String getMessage(Throwable e){
         String msg = e.getMessage();
         msg += "\n";
